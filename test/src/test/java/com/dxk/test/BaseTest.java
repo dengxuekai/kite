52a8039424eafd88f66ff76ed16c365e099444e8
kai <dengxuekai@meeruu.com>
2019-06-12 15:04:34 +0800

    修改springboot版本

--- test/src/test/java/com/dxk/test/BaseTest.java
+++ test/src/test/java/com/dxk/test/BaseTest.java
@@ -7,8 +7,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * @author hzdengxuekai
@@ -21,6 +25,7 @@ public class BaseTest {
 
 	@Resource
 	private TestService testService;
+	Random random = new Random();
 
 	@Test
 	public void test() {
@@ -36,5 +41,26 @@ public class BaseTest {
 		testService.setStudent("str", student);
 		Student str = testService.getStudent("str");
 		log.info(">>>>> str :{}", str);
+	}
+
+	@Test
+	public void test2() {
+		int number = 10000000;
+		List<Double> list = new ArrayList<>(number);
+		for (int i = 0; i < number; i ++) {
+			double tem = random.nextDouble();
+			if (tem < 0.10000000000D) {
+				list.add(tem);
+			}
+		}
+		log.info(">>>>> str :{}", list.size() / (float) number);
+//		log.info(">>>>> str :{}", list.toString());
+	}
+
+	@Test
+	public void test3() {
+		for (int i = 0; i < 10; i ++) {
+			test2();
+		}
 	}
 }
